a6afd59d3df28b0e5346d9b087049d7fb138bc0c
vishwashdark <vishsharma2003@gmail.com>
2025-01-01 11:19:24 +0530

    1st initialization

---
+++ script.js
@@ -0,0 +1,11 @@
+new fullpage('#fullPage', {
+    autoScrolling: true, // Enable auto-scrolling
+    navigation: true, // Show side navigation
+    anchors: ['section1', 'section2', 'section3'], // Section anchor names
+    navigationTooltips: ['Primera Sección', 'Segunda Sección', 'Tercera Sección'], // Tooltip names
+    showActiveTooltip: true, // Highlight active section
+    scrollingSpeed: 2000, // Set scroll speed
+    controlArrows: false, // Disable slide control arrows
+    slidesNavigation: true, // Enable bottom slide navigation
+  });
+
